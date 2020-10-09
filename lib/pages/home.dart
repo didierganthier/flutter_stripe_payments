9a9d34e82270a72c2db565141504223eae75d366
didierganthier <didierganthierperan@gmail.com>
2020-10-09 18:05:20 -0400

    home.dart two payment options added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -6,15 +6,58 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  onItemPress(BuildContext context, int index){
+    print('index: ${index.toString()}');
+
+    switch(index){
+      case 0:
+        //pay via new card
+        break;
+      case 1:
+        Navigator.pushNamed(context, '/existing-cards');
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Choose existing card"),
       ),
       body: Container(
         padding: EdgeInsets.all(20),
-        child: Text("Home Page"),
+        child: ListView.separated(
+          itemBuilder: (context, index){
+            Icon icon;
+            Text text;
+
+            switch(index){
+              case 0:
+                icon = Icon(Icons.add_circle, color: theme.primaryColor);
+                text = Text("Pay via new card");
+                break;
+              case 1:
+                icon = Icon(Icons.credit_card, color: theme.primaryColor);
+                text = Text("Pay via existing card");
+                break;
+            }
+
+            return InkWell(
+              onTap:(){onItemPress(context, index);},
+              child: ListTile(
+                title: text,
+                leading: icon,
+              ),
+            );
+          },
+          separatorBuilder: (context, index) => Divider(
+            color: theme.primaryColor,
+          ),
+          itemCount: 2,
+        ),
       ),
     );
   }
