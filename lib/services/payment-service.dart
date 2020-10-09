0b267365c62ee557f83b68e9d25558dee791fe01
didierganthier <didierganthierperan@gmail.com>
2020-10-09 19:01:04 -0400

    payment-service.dart basic implementation

---
+++ lib/services/payment-service.dart
@@ -0,0 +1,25 @@
+class StripeTransactionResponse {
+  String message;
+  bool success;
+  StripeTransactionResponse({this.message, this.success});
+}
+
+class StripeService {
+  static String apiBase = 'https://api.stripe.com//v1';
+  static String secret = '';
+
+  static init(){
+
+  }
+
+  static StripeTransactionResponse payViaExistingCard({String amount, String currency, card}){
+    return StripeTransactionResponse(
+      message: 'Transaction successful',
+      success: true,
+    );
+  }
+
+  static StripeTransactionResponse payWithNewCard({String amount, String currency}){
+
+  }
+}
