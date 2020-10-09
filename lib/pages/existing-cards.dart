46edc171df4a910b4e943de17c021c9bed9ef48a
didierganthier <didierganthierperan@gmail.com>
2020-10-09 18:26:39 -0400

    credit-card UI displayed in existing-cards.dart

--- lib/pages/existing-cards.dart
+++ lib/pages/existing-cards.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/credit_card_widget.dart';
 
 class ExistingCardsPage extends StatefulWidget {
   @override
@@ -6,6 +7,27 @@ class ExistingCardsPage extends StatefulWidget {
 }
 
 class _ExistingCardsPageState extends State<ExistingCardsPage> {
+  List cards = [
+    {
+      'cardNumber': '4242424242424242',
+      'expiryDate': '04/24',
+      'cardHolderName': 'Muhammad Ahsan Ayaz',
+      'cvvCode': '424',
+      'showBackView': false,
+    },
+    {
+      'cardNumber': '3566002020360505',
+      'expiryDate': '04/23',
+      'cardHolderName': 'Tracer',
+      'cvvCode': '123',
+      'showBackView': false,
+    }
+  ];
+
+  payViaExistingCard(BuildContext context, card){
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,7 +36,24 @@ class _ExistingCardsPageState extends State<ExistingCardsPage> {
       ),
       body: Container(
         padding: EdgeInsets.all(20),
-        child: Text("existing cards page"),
+        child: ListView.builder(
+          itemCount: cards.length,
+          itemBuilder: (BuildContext context, int index) {
+            var card = cards[index];
+            return InkWell(
+              onTap:(){
+                payViaExistingCard(context, card);
+              },
+              child: CreditCardWidget(
+                cardNumber: card['cardNumber'],
+                expiryDate: card['expiryDate'],
+                cardHolderName: card['cardHolderName'],
+                cvvCode: card['cvvCode'],
+                showBackView: card['showBackView'],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
